940435d72c2c30aec18acdbaaac028a0c8ec6986
Nardos221 <nardosayele9@gmail.com>
2022-08-07 13:47:53 +0300

    4th commit
    fourth week practice

---
+++ practice/practice/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = txt_username.Text;
+            string password = txt_password.Text;
+            if (username == "admin" & password == "password")
+            {
+                MessageBox.Show("Valid login");
+                Form1 form1 = new Form1();
+                Hide();
+                form1.Show();
+            }
+        }
+    }
+}
